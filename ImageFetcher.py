91a67b911b09072edfe3781a30fbc744690a786a
Ali Siddiqui <alinsiddiqui@gmail.com>
2017-03-11 12:22:58 -0800

    Add ability to fetch links from all text files in the directory

--- ImageFetcher.py
+++ ImageFetcher.py
@@ -1,23 +1,31 @@
 import urllib
 from PIL import Image
 import os
+import glob
 
-class_name = 'Apple'
+for file in glob.glob("*.txt"):
 
-f = open(class_name + '.txt', 'r')
+    class_name = os.path.splitext(file)[0]
 
-i = 0
-for line in f:
-    file_path = 'data/' + class_name + '/' + str(i) + '.jpg'
-    print('Fetching {0}'.format(file_path))
+    directory = 'data/' + class_name + '/'
 
-    try:
-        urllib.urlretrieve(line, file_path)
-        im = Image.open(file_path)
-        im.load()
-        i += 1
-    except Exception as e:
-        print('Error fetching')
-        if os.path.isfile(file_path):
-            print('Removing {0}'.format(file_path))
-            os.remove(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    f = open(class_name + '.txt', 'r')
+
+    i = 0
+    for line in f:
+        file_path = directory + str(i) + '.jpg'
+        print('Fetching {0}'.format(file_path))
+
+        try:
+            urllib.urlretrieve(line, file_path)
+            im = Image.open(file_path)
+            im.load()
+            i += 1
+        except Exception as e:
+            print('Error fetching')
+            if os.path.isfile(file_path):
+                print('Removing {0}'.format(file_path))
+                os.remove(file_path)
