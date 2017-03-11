1250601475f85d145d16f90ed21b679ead2b29b1
mattpaletta <mattpaletta@gmail.com>
2017-03-11 13:47:46 -0800

    Added Xcode Project

---
+++ Ripe/Ripe/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Ripe
+//
+//  Created by Matthew Paletta on 2017-03-11.
+//  Copyright © 2017 Matthew Paletta. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
