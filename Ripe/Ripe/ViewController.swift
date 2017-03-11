e1a63abbf2d3f3b59db19301ffb47865c92175be
mattpaletta <mattpaletta@gmail.com>
2017-03-11 14:04:31 -0800

    Recreated project

--- Ripe/Ripe/ViewController.swift
+++ Ripe/Ripe/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  DataViewController.swift
+//  ViewController.swift
 //  Ripe
 //
 //  Created by Matthew Paletta on 2017-03-11.
@@ -8,11 +8,7 @@
 
 import UIKit
 
-class DataViewController: UIViewController {
-
-    @IBOutlet weak var dataLabel: UILabel!
-    var dataObject: String = ""
-
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +18,6 @@ class DataViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.dataLabel!.text = dataObject
     }
 
 
