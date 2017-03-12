2251856f1d6d07f20ff0a52d95f8e727392041a7
mattpaletta <mattpaletta@gmail.com>
2017-03-11 22:33:42 -0800

    Working on Json Parsing

--- Ripe/RipeTests/RipeTests.swift
+++ Ripe/RipeTests/RipeTests.swift
@@ -21,16 +21,27 @@ class RipeTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testParsing() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        if let produceList
+            = Dictionary<String, AnyObject>.loadJSONFromBundle() {
+            //print(produceList)
+            let parsed = produceList.dictToProduce()
+            
+            XCTAssert(parsed.count == 16)
+            XCTAssert(parsed == parsed.sorted(by: {$0.name > $1.name}))
+        }
     }
     
+    /*
     func testPerformanceExample() {
         // This is an example of a performance test case.
+        
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
+    */
 }
